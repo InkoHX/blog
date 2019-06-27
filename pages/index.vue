33e6f7be02d0889b8d404f78f4bef382682277b7
InkoHX <inkohx@gmail.com>
2019-06-28 00:06:41 +0900

    Fix lint

--- pages/index.vue
+++ pages/index.vue
@@ -8,10 +8,12 @@
           column
         >
           <v-avatar size="256px">
-            <img src="https://avatars1.githubusercontent.com/u/33122816?s=460&v=4"/>
+            <img src="https://avatars1.githubusercontent.com/u/33122816?s=460&v=4">
           </v-avatar>
           <br>
-          <h1 class="display-1 text-xs-center mb-2">InkoHX</h1>
+          <h1 class="display-1 text-xs-center mb-2">
+            InkoHX
+          </h1>
         </v-layout>
       </v-parallax>
     </section>
