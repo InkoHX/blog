a8ca5af8dd4b97058e979468b0075ae8ba25966f
InkoHX <inkohx@gmail.com>
2019-06-28 00:03:31 +0900

    base

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: ['@typescript-eslint'],
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: '@typescript-eslint/parser'
+  },
+  extends: [
+    '@nuxtjs',
+    'plugin:nuxt/recommended'
+  ],
+  // add your custom rules here
+  rules: {
+    '@typescript-eslint/no-unused-vars': 0
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="home">
+    <section>
+      <v-parallax src="https://i.imgur.com/7BzjiR8.png" height="700">
+        <v-layout
+          justify-center
+          align-center
+          column
+        >
+          <v-avatar size="256px">
+            <img src="https://avatars1.githubusercontent.com/u/33122816?s=460&v=4"/>
+          </v-avatar>
+          <br>
+          <h1 class="display-1 text-xs-center mb-2">InkoHX</h1>
+        </v-layout>
+      </v-parallax>
+    </section>
+  </div>
+</template>
