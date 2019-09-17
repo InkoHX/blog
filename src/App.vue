20bf4d9d8e0746931f514190cb3f96b00a350a6b
InkoHX <inkohx@gmail.com>
2019-09-18 01:03:35 +0900

    Add App.vue

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+    <Navbar />
+    <Parallax />
+    <Content />
+  </v-app>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+import Content from '@/components/core/Content'
+import Footer from '@/components/core/Footer'
+import Parallax from '@/components/core/Parallax'
+import Navbar from '@/components/core/Navbar'
+
+@Component({
+  components: {
+    Parallax,
+    Content,
+    Footer,
+    Navbar
+  }
+})
+export default class App extends Vue {
+
+}
+</script>
