5525c7134acb13b29cf4a01ace30334ca1dfa8b6
InkoHX <inkohx@gmail.com>
2019-09-18 01:05:20 +0900

    Add Parallax.vue

---
+++ src/components/core/Parallax.vue
@@ -0,0 +1,29 @@
+<template>
+  <section>
+    <v-parallax src="@/assets/coding.png" height="700">
+      <v-layout justify-center align-center column>
+        <v-avatar size="256">
+          <img src="@/assets/inkohx.png" />
+        </v-avatar>
+        <br />
+        <h1><vue-typer text="Hello, My name is InkoHX!" /></h1>
+      </v-layout>
+    </v-parallax>
+  </section>
+</template>
+
+<style lang="scss">
+.vue-typer .custom.char.typed {
+  color: white;
+}
+
+.vue-typer .custom.caret {
+  display: none;
+}
+
+h1 {
+  font-family: "Courier New", Courier, monospace;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
