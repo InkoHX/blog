e51eec535ce46752fa674482b79bf5472669ad5c
InkoHX <inkohx@gmail.com>
2019-09-18 01:04:51 +0900

    Add Navbar.vue

---
+++ src/components/core/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="navbar">
+    <v-app-bar app dense dark scroll-off-screen>
+      <v-app-bar-nav-icon @click="toggleNavigation()">
+        <v-icon>reorder</v-icon>
+      </v-app-bar-nav-icon>
+      <v-toolbar-title>InkoHX - Portfolio</v-toolbar-title>
+    </v-app-bar>
+    <v-navigation-drawer v-model="navigation" bottom temporary fixed>
+      <v-list-item two-line>
+        <v-list-item-avatar>
+          <v-img src="@/assets/inkohx.png" />
+        </v-list-item-avatar>
+        <v-list-item-content>
+          <v-list-item-title>InkoHX</v-list-item-title>
+          <v-list-item-subtitle>ただの学生だよ！</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class Navbar extends Vue {
+  private navigation: boolean = false;
+
+  public hasNavigation (): boolean {
+    return this.navigation
+  }
+
+  public toggleNavigation (): void {
+    this.navigation = !this.navigation
+  }
+}
+</script>
