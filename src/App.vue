1ba378ec35fe224daa7c0999d941966d9dd50f28
InkoHX <inkohx@gmail.com>
2019-09-19 22:12:59 +0900

    Footer (適当)

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <Navbar />
     <Parallax />
     <Content />
+    <Footer />
   </v-app>
 </template>
 
@@ -12,12 +13,14 @@ import { Component, Vue } from 'vue-property-decorator'
 import Content from '@/components/core/Content.vue'
 import Parallax from '@/components/core/Parallax.vue'
 import Navbar from '@/components/core/Navbar.vue'
+import Footer from '@/components/core/Footer.vue'
 
 @Component({
   components: {
     Parallax,
     Content,
-    Navbar
+    Navbar,
+    Footer
   }
 })
 export default class App extends Vue {
---
+++ src/components/core/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-footer padless>
+    <v-card width="100%">
+      <v-card-text class="text-center">
+        <strong>Made with InkoHX</strong>
+      </v-card-text>
+    </v-card>
+  </v-footer>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class Footer extends Vue {}
+</script>
