acf7db568a7c835eab19822351a67cfd72abe193
InkoHX <inkohx@gmail.com>
2020-05-08 22:44:18 +0900

    feat: add viewport

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -22,6 +22,7 @@ const App = (props: AppProps): JSX.Element => {
     <React.Fragment>
       <Head>
         <title>InkoHX | Portfolio</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@200;300;400;500;600;700&display=swap" rel="stylesheet" />
       </Head>
 
