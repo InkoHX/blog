c02067ab67dbe532a55b31113a245691c940dde0
InkoHX <inkohx@gmail.com>
2020-05-08 13:45:58 +0900

    Move to ReactJS

---
+++ src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import { AppProps } from 'next/app'
+import * as React from 'react'
+import { ThemeProvider } from 'styled-components'
+
+const theme = {
+  colors: {
+    primary: '#0070f3'
+  }
+}
+
+const App = (props: AppProps): JSX.Element => {
+  const { Component, pageProps } = props
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
+}
+
+export default App
---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  color: #117;
+`
+
+const IndexPage = (): JSX.Element => {
+  return (
+    <Title>久しぶりのReact</Title>
+  )
+}
+
+export default IndexPage
