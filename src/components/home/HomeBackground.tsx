94a5cf1a466f56ac343001d93faabd71af37f0ac
InkoHX <inkohx@gmail.com>
2020-05-10 17:15:48 +0900

    fix: graident color

--- src/components/home/HomeBackground.tsx
+++ src/components/home/HomeBackground.tsx
@@ -6,8 +6,16 @@ const keyframe = keyframes`
   to: { background-position: 0% 100%; }
 `
 
+const gradientColors = [
+  '#790e8b',
+  '#6a0080',
+  '#5c007a',
+  '#4a0072',
+  '#38006b'
+]
+
 export const HomeBackground = styled.div`
-  background: linear-gradient(45deg, #7c43bd, #4a148c, #38006b);
+  background: linear-gradient(45deg, ${gradientColors.join()});
   background-size: 500% 500%;
   animation: ${keyframe} 10s ease infinite;
   padding: 200px 30px;
