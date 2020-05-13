df417ac65f1ced4c6cf1f2e1cc876e4efbb66f52
InkoHX <inkohx@gmail.com>
2020-05-13 19:51:41 +0900

    fix: type error

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,13 +1,5 @@
 import { Theme } from '@material-ui/core'
 
-declare module 'remark-html' {
-  export default function (): any
-}
-
-declare module 'remark-highlight.js' {
-  export default function (): any
-}
-
 declare module 'styled-components' {
   // eslint-disable-next-line @typescript-eslint/no-empty-interface
   interface DefaultTheme extends Theme {}
--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,11 +1,13 @@
 import globCallback from 'glob'
-import remarkHighlight from 'remark-highlight.js'
-import remarkHtml from 'remark-html'
 import remarkParse from 'remark-parse'
 import unified from 'unified'
 import util from 'util'
 import { VFile } from 'vfile'
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const remarkHighlight = require('remark-highlight.js')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const remarkHtml = require('remark-html')
 const globAsync = util.promisify(globCallback)
 
 export function parseMarkdownToHTML (content: string): Promise<VFile> {
