95852292af258b58ea3ddb9bd3580868c1f8cd0f
InkoHX <inkohx@gmail.com>
2020-05-16 00:33:26 +0900

    feat: フォントを配置

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -32,7 +32,7 @@ const App = (props: AppProps): JSX.Element => {
       />
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+        <link rel="stylesheet" href="/style/roboto.css" />
       </Head>
 
       <StylesProvider injectFirst>
