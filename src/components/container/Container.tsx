41264b85481662898065e723aec932e3e999a42f
InkoHX <inkohx@gmail.com>
2020-05-22 19:11:24 +0900

    fix: AppBarによって要素が見えなくなるのを修正

---
+++ src/components/container/Container.tsx
@@ -0,0 +1,6 @@
+import { Container as MaterialContainer } from '@material-ui/core'
+import styled from 'styled-components'
+
+export const Container = styled(MaterialContainer)`
+  padding-top: 60px;
+`
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export * from './footer'
 export * from './blog'
 export * from './appbar'
+export * from './container'
