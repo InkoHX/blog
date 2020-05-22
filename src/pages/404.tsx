3fc6b842bbe5b3de28a1c67aa5f71fdc2b524814
InkoHX <inkohx@gmail.com>
2020-05-22 19:56:19 +0900

    feat: add custom 404 page.

---
+++ src/pages/404.tsx
@@ -0,0 +1,20 @@
+import { Typography } from '@material-ui/core'
+import styled from 'styled-components'
+
+const ErrorPageContainer = styled.div`
+  height: 100vh;
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+`
+
+const ErrorPage: React.FC = () => {
+  return (
+    <ErrorPageContainer>
+      <Typography variant='h5'>404 | ページが見つかりませんでした。</Typography>
+    </ErrorPageContainer>
+  )
+}
+
+export default ErrorPage
